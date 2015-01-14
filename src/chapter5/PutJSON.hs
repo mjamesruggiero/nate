428124da3d7804f03aef93aff61c6db840c2d1e1
mjamesruggiero <mjamesruggiero@gmail.com>
2015-01-13 20:48:09 -0800

    starting ch 5

---
+++ src/chapter5/PutJSON.hs
@@ -0,0 +1,14 @@
+module PutJSON where
+
+import Data.List (intercalate)
+import SimpleJSON
+
+renderJValue :: JValue -> String
+
+renderJValue (JString s) = show s
+renderJValue (JNumber n) = show n
+renderJValue (JBool True) = "true"
+renderJValue (JBool False) = "false"
+renderJValue JNull = "null"
+
+renderJValue (JObject o) = "false"
