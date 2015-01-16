fccec3f6150778808c7fb5bb54cfdf08ee1da320
mjamesruggiero <mjamesruggiero@gmail.com>
2015-01-15 19:46:06 -0800

    pretty-printing JSON

--- src/chapter5/PutJSON.hs
+++ src/chapter5/PutJSON.hs
@@ -11,4 +11,14 @@ renderJValue (JBool True) = "true"
 renderJValue (JBool False) = "false"
 renderJValue JNull = "null"
 
-renderJValue (JObject o) = "false"
+renderJValue (JObject o) = "{" ++ pairs o ++ "}"
+    where pairs [] = ""
+          pairs ps = intercalate ", " (map renderPair ps)
+          renderPair (k,v) = show k ++ ": " ++ renderJValue v
+
+renderJValue (JArray a) = "[" ++ values a ++ "]"
+    where values [] = ""
+          values vs = intercalate ", " (map renderJValue vs)
+
+putJValue :: JValue -> IO ()
+putJValue v = putStrLn (renderJValue v)
