3f6ee5105d19c2c3bbde1ad38440d3e3bf371d4e
mjamesruggiero <mjamesruggiero@gmail.com>
2015-01-28 21:46:54 -0800

    almost done

--- src/chapter6/JSONClass.hs
+++ src/chapter6/JSONClass.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 module JSONClass
     (
       JSON(..)
@@ -38,6 +39,27 @@ instance JSON JValue where
     fromJValue = Right
 
 instance JSON Bool where
-    toJValue = JBool
+    toJValue             = JBool
     fromJValue (JBool b) = Right b
-    fromJValue _ = Left "Not a JSON boolean"
+    fromJValue _         = Left "Not a JSON boolean"
+
+instance JSON String where
+    toJValue               = JString
+    fromJValue (JString s) = Right s
+    fromJValue _           = Left "not a JSON string"
+
+doubleToJValue :: (Double -> a) -> JValue -> Either JSONError a
+doubleToJValue f (JNumber v) = Right (f v)
+doubleToJValue _ _ = Left "not a JSON number"
+
+instance JSON Int where
+    toJValue = JNumber . realToFrac
+    fromJValue = doubleToJValue round
+
+instance JSON Integer where
+    toJValue = JNumber . realToFrac
+    fromJValue = doubleToJValue round
+
+instance JSON Double where
+    toJValue = JNumber
+    fromJValue = doubleToJValue id
