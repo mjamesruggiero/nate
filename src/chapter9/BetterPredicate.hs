c59ecd0a10072cc7b9fbe4c680f1f204217a3d1e
mjamesruggiero <mjamesruggiero@gmail.com>
2015-02-13 23:11:59 -0800

    WIP better predicate

---
+++ src/chapter9/BetterPredicate.hs
@@ -0,0 +1,26 @@
+import Control.Monad (filterM)
+import System.Directory (Permissions(..), getModificationTime, getPermissions)
+import System.Time (ClockTime(..))
+import System.FilePath (takeExtension)
+import Control.Exception (bracket, handle)
+import System.IO (IOMode(..), hClose, hFileSize, openFile)
+
+-- the function we wrote yesterday
+import RecursiveContents (getRecursiveContents)
+
+type Predicate = FilePath        -- path to directory entry
+               -> Permissions    -- permissions
+               -> Maybe Integer  -- file size (Nothing if not file)
+               -> ClockTime      -- last modified
+               -> Bool
+
+getFileSize :: FilePath -> IO (Maybe Integer)
+
+betterFind :: Predicate -> FilePath -> IO [FilePath]
+
+betterFind p path = getRecursiveContents path >>= filterM check
+    where check name = do
+            perms <- getPermissions name
+            size <- getFileSize name
+            modified <- getModificationTime name
+            return (p name perms size modified)
