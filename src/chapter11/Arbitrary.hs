8da9532c8493251a954b156316be3993ebec12e6
mjamesruggiero <mjamesruggiero@gmail.com>
2015-03-12 19:55:15 -0700

    arbitrary

---
+++ src/chapter11/Arbitrary.hs
@@ -0,0 +1,17 @@
+module Arbitrary where
+
+import Prelude hiding (Bool(..))
+import Test.QuickCheck
+
+data Ternary = Yes
+             | No
+             | Unknown
+             deriving(Eq, Show)
+
+instance Arbitrary Ternary where
+    arbitrary = do
+        n <- choose(0, 2) :: Gen Int
+        return $ case n of
+                      0 -> Yes
+                      1 -> No
+                      _ -> Unknown
